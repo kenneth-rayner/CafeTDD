8d0984b309d2ba29d67f7b1c4d5710660a68da40
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-03-14 11:29:01 +0000

    Add second test for grinding coffee.

--- src/test/scala/CafeSpec.scala
+++ src/test/scala/CafeSpec.scala
@@ -5,9 +5,14 @@ class CafeSpec extends WordSpec with MustMatchers{
 
   "Cafe" must {
 
-    "Will return '40' when w=20 input" in {
+    "Will return '40' when 25 input" in {
 
       Cafe.heat(25) mustEqual 40
+    }
+
+    "Will return 'Finished grinding coffee'" in {
+
+      Cafe.grind("Arabica Beans") mustEqual "Finished grinding coffee"
     }
 
   }
