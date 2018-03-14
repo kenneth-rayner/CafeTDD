ef0b40d3a80713aee54e2030a18714ab767d0fa1
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-03-14 14:19:40 +0000

    Refactor heat function, add to brew function.

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -1,7 +1,5 @@
-
-
 object Cafe {
-  case class Water (temperature: Double = 0D)
+  case class Water(temperature: Double = 0)
 
   type CoffeeBeans = String
   type Milk = String
@@ -9,13 +7,14 @@ object Cafe {
   type FrothedMilk = String
   type Coffee = String
 
-  def heat (water: Water, temperature: Double =40D ): Water = {
+  case class BrewingException(msg: String) extends Exception(msg)
 
-    temperature match {
-      case t if t == 0D =>water.copy(40D)
-      case _  => water.copy(temperature)
+  def heat (water: Water): Water = {
+
+    water.temperature match {
+      case t if t == 0 =>water.copy(40)
+      case _  => water.copy(water.temperature)
     }
-
   }
 
   def grind(beans : CoffeeBeans) : GroundCoffee = {
@@ -24,8 +23,8 @@ object Cafe {
       case b if b == "Arabica Beans" => "Finished grinding coffee"
       case _ => "Those are not Arabica Beans"
     }
-
   }
+
   def frothMilk(milk: Milk): FrothedMilk = {
 
     milk match {
@@ -34,6 +33,10 @@ object Cafe {
     }
   }
   def brew(water: Water, coffee: GroundCoffee): Coffee = {
-    "Coffee has been brewed"
+
+    water.temperature match {
+      case t if t < 40 => throw new BrewingException("The water is too cold")
+      case _ => "Coffee has been brewed"
+    }
   }
 }
--- src/test/scala/CafeSpec.scala
+++ src/test/scala/CafeSpec.scala
@@ -1,24 +1,23 @@
-import Cafe.Water
-import Cafe.GroundCoffee
+import Cafe.{BrewingException, GroundCoffee, Water}
 import org.scalatest.{MustMatchers, WordSpec}
 
-class CafeSpec extends WordSpec with MustMatchers{
+class CafeSpec extends WordSpec with MustMatchers {
 
 
   "Cafe" must {
 
     "Will return '40' when 25 input" in {
 
-      Cafe.heat(Water(25D)) mustEqual Water(40D)
+      Cafe.heat(Water(25)) mustEqual Water(25)
     }
 
     "Will return '40' when 20 input" in {
 
-      Cafe.heat(Water(20D)) mustEqual Water(40D)
+      Cafe.heat(Water(20)) mustEqual Water(20)
     }
     "Will return '40' when nothing input" in {
 
-      Cafe.heat(Water()) mustEqual Water(40D)
+      Cafe.heat(Water()) mustEqual Water(40)
     }
 
     "Will return 'Finished grinding coffee'" in {
@@ -30,16 +29,25 @@ class CafeSpec extends WordSpec with MustMatchers{
       Cafe.frothMilk("WholeMilk") mustEqual "Milk has been frothed"
     }
     "Will return 'Illegal argument exception'when semi skimmed milk is input" in {
-val e = intercept[IllegalArgumentException] {
+      val e = intercept[IllegalArgumentException] {
 
-  Cafe.frothMilk("Semi SkimmedMilk")
-  }
-e.getMessage mustEqual "Should use whole milk"
-}
-  }
-    "Will return 'Coffee has been brewed" in {
-      Cafe.brew(Water(), "Ground Coffee") mustEqual "Coffee has been brewed"
+        Cafe.frothMilk("Semi SkimmedMilk")
+      }
+      e.getMessage mustEqual "Should use whole milk"
     }
+  }
+  "Will return 'Coffee has been brewed" in {
+
+    Cafe.brew(Water(40), "Ground Coffee") mustEqual "Coffee has been brewed"
+  }
+
+  "Will return 'Brewing exception' when water input is less than 40D" in {
+    val e = intercept[BrewingException] {
+
+      Cafe.brew(Water(20), "Ground Coffee")
+    }
+    e.getMessage mustEqual "The water is too cold"
+  }
 
 
 }
